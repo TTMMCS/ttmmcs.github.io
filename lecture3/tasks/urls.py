ce22f1f7b8134a9eae04c5b71bbd91dc12d9b1ed
tungmaimcs <tungmaimcs@gmail.com>
2022-01-19 21:23:22 +0700

    test new html

--- lecture3/tasks/urls.py
+++ lecture3/tasks/urls.py
@@ -3,7 +3,7 @@ from django.urls.resolvers import URLPattern
 
 from . import views
 
-app_name = "tasks"
+app_name = "apptasks"
 urlpatterns = [
     path("", views.index, name = "index"),
     path("add", views.add, name = "adds")
--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -8,15 +8,10 @@ from django.urls import reverse
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="Priority",min_value=1,max_value=10)
+    priority = forms.IntegerField(label="Priority",min_value=1,max_value=5)
 
 # Create your views here.
-def index(request):
-    if "tasks" not in request.session:
-        request.session["tasks"] = []
-    return render(request, "tasks/index.html", {
-        "tasks": request.session["tasks"]
-    })
+
 
 def add(request):
     if request.method == "POST":
@@ -24,7 +19,7 @@ def add(request):
         if form.is_valid():
             task = form.cleaned_data["task"]
             request.session["tasks"] += [task]
-            return HttpResponseRedirect(reverse("tasks:index"))
+            return HttpResponseRedirect(reverse("apptasks:index"))
         else:
             return render(request, "tasks/add.html", {
                 "form": form
@@ -33,5 +28,10 @@ def add(request):
         "form": NewTaskForm()
     })
 
-
+def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+    return render(request, "tasks/index.html", {
+        "tasks": request.session["tasks"]
+    })
 
